4603cd1dc1a65bbdadacb4b695850c6924796702
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-11-08 20:43:52 +0100

    Lektion 1 prepp

---
+++ Lektion1.go
@@ -0,0 +1,45 @@
+// Grundprogram
+package main
+
+import (
+	"fmt"
+	//"bufio"
+	//"os"
+	//"strconv"
+)
+
+func main() {
+	fmt.Printf("Hello world")
+
+}
+
+// Variabler
+// Static typing - variabel kan inte ändra datatyp
+// var variabel_namn datatyp = värde
+// Behöver inte ge värde direkt
+
+// Datatyper
+// integer, float, string
+
+// Implicit - låta go "välja" typen
+// Explicit - bestämma typen direkt
+// fmt.Printf("%T", number)
+// Walrus - skippa var
+
+// Printf
+//Println
+
+// Console input
+// Type conversion
+
+/* scanner := bufio.NewScanner(os.Stdin)
+scanner.Scan()
+input := scanner.Text()
+scanner.Scan()
+input, _ := strconv.ParseInt(scanner.Text(), 10, 64) */
+
+// Operators
+/*
++ - / * %
+import "math"
+*/
