4a139cd37f7c0283bbab3be9b0bc6dcc3429b644
HampusEriksson <hampuse1@gmail.com>
2022-11-11 10:11:38 +0100

    Lektion 2 done

--- Lektion2.go
+++ Lektion2.go
@@ -1,30 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 
 	x := 5
 	val := x < 5
-	fmt.Printf("%t", val)
+	fmt.Printf("%t\n", val)
+	// Lägg parenteser vid flera conditions
+	fmt.Println(x == 5 || x != 5 && x == 5)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("What's your age?")
+	scanner.Scan()
+	user_age, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	if user_age > 65 {
+		fmt.Println("You are retired")
+	} else if user_age >= 18 {
+		fmt.Println("You are an adult")
+
+	} else if user_age > 13 {
+		fmt.Println("You are a youth")
+
+	} else {
+		fmt.Println("You are a baby")
+	}
+	// While - vad är det???
+
+	y := 5
+
+	for y < 10 {
+		y++
+		fmt.Println(y)
+	}
+
+	for z := 0; z < 5; z++ {
+		fmt.Println(z)
+	}
+
+	fmt.Println("Vad heter du?")
+	scanner.Scan()
+	name := scanner.Text()
+
+	switch name {
+
+	case "Jubin":
+		fmt.Println("Hej")
+
+	case "Leon":
+		fmt.Println("Nej")
+
+	case "Daniel":
+		fmt.Println("Tåååg")
+
+	default:
+		fmt.Println("Inget case assignat")
+	}
+
 }
 
 // Samma jämförelser som Python. < > osv
+// < > <= >= != ==
 // Kan inte jämföra float och int
 // AND &&
 // OR ||
 // NOT !
-// true || false && true
 // If
 // Else if
 // Else
 // For
-// While - vad är det???
 // for x:=0; x<5; x++
 // break & continue - samma som python
-// % stuff
 // Switch
 // case x:
 // default:
-//
-//
