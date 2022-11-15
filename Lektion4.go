7976b911609bb0043c3f6eb42058795bde6045e4
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-11-15 20:18:27 +0100

    Create Lektion4.go

---
+++ Lektion4.go
@@ -0,0 +1,24 @@
+package main
+
+// Funktionen main körs när filen körs
+func main() {
+
+}
+
+// Skapa en funktion
+
+// Kalla på en funktion
+
+// Parametrar skrivs som (variabelnamn datatyp)
+// (variabelnamn datatyp, variabelnamn datatyp)
+// (variabelnamn, variabelnamn datatyp)
+// behöver inte säga return x,y
+// defer - görs i slutet
+// funktion är en datatyp. kan t.ex. sparas i variabel eller returneras
+/*test := func(){
+fmt.Println("Testing")
+}*/
+
+// test2(myFunc func(int) int){}
+
+// closures
