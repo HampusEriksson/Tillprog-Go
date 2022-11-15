6af4294a0e01e04b2166194e39c31757fb2285a7
HampusEriksson <hampuse1@gmail.com>
2022-11-15 14:07:33 +0100

    Lektion 3 done

---
+++ test.py
@@ -0,0 +1,7 @@
+min_lista = ["charader", "bajs", "despair", 69, ["charader", "bajs", "despair"]]
+print(min_lista)
+
+students = ["Jorge", "Leon", "Vigor", "Jubin"]
+
+for i in range(len(students)):
+    print(students[i])
