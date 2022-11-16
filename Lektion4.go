3490adbb65f87210ba42684ae7b16a637fa00fa7
HampusEriksson <hampuse1@gmail.com>
2022-11-16 12:46:05 +0100

    Lektion 4 done

--- Lektion4.go
+++ Lektion4.go
@@ -1,24 +1,58 @@
 package main
 
-// Funktionen main körs när filen körs
-func main() {
-
-}
+import (
+	"fmt"
+	"math"
+)
 
 // Skapa en funktion
-
-// Kalla på en funktion
-
 // Parametrar skrivs som (variabelnamn datatyp)
 // (variabelnamn datatyp, variabelnamn datatyp)
 // (variabelnamn, variabelnamn datatyp)
-// behöver inte säga return x,y
-// defer - görs i slutet
-// funktion är en datatyp. kan t.ex. sparas i variabel eller returneras
-/*test := func(){
-fmt.Println("Testing")
-}*/
+func pythagoras(a, b float64) float64 {
+	//math.Pow(2, 10)
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 
-// test2(myFunc func(int) int){}
+// defer - görs i slutet
+// behöver inte säga return x,y
+func sums(a, b int) (sum int) {
+	defer fmt.Println("Nu är funktionen returnerad")
+	fmt.Println("Vi ska summera", a, b)
+	sum = a + b
+	return
+}
 
 // closures
+
+func check_runs() func() {
+	runs := 0
+
+	return func() {
+		runs++ // ++ ökar variabeln med 1
+		fmt.Println(runs)
+	}
+}
+
+// Funktionen main körs när filen körs
+func main() {
+	fmt.Println("Detta körs när filen körs.")
+	// Kalla på en funktion
+
+	fmt.Println(pythagoras(2, 3))
+	fmt.Println(sums(5, 6))
+
+	// funktion är en datatyp. kan t.ex. sparas i variabel eller returneras
+	test := func() {
+		fmt.Println("Testing")
+	}
+
+	test()
+	test()
+
+	get_my_runs := check_runs()
+	get_my_runs()
+	get_my_runs()
+	get_my_runs()
+
+}
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-min_lista = ["charader", "bajs", "despair", 69, ["charader", "bajs", "despair"]]
-print(min_lista)
-
-students = ["Jorge", "Leon", "Vigor", "Jubin"]
-
-for i in range(len(students)):
-    print(students[i])
+def f(x):
+    return x**2
