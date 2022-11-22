2b4ff6019ce61a660da3956a1e7f07029a596c8e
HampusEriksson <hampuse1@gmail.com>
2022-11-22 17:06:03 +0100

    Lektion 5 prepp

---
+++ Lektion5.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+}
+
+// Mutable - kan ändras
+// int - exempel
+// slice - exempel
+/*var x []int = []int{3, 4, 5}
+y := x // y och x pekar på samma slice
+y[0] = 100
+fmt.Println(y)*/
+// slice och maps beter sig "konstigt" - de pekar på en minnesplats
+// array skapar en kopia
+// Immutable
+//
