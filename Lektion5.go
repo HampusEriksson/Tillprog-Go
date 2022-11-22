e29ce6cfa1a5df4bb13c997156d1b020d540d775
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-11-22 21:59:56 +0100

    Lektion 5 prepp

--- Lektion5.go
+++ Lektion5.go
@@ -1,6 +1,15 @@
+// https://www.youtube.com/watch?v=2ybLD6_2gKM&ab_channel=LowLevelLearning
+
 package main
 
+import "fmt"
+
 func main() {
+	toChange := "Hello"
+	var pointer *string = &toChange
+	fmt.Println(pointer, &pointer, *pointer)
+	*pointer = "Good bye"
+	fmt.Println(pointer, &pointer, *pointer)
 
 }
 
@@ -12,6 +21,10 @@ y := x // y och x pekar på samma slice
 y[0] = 100
 fmt.Println(y)*/
 // slice och maps beter sig "konstigt" - de pekar på en minnesplats
+// exempel - skicka in slice i en funktion
 // array skapar en kopia
 // Immutable
-//
+// Pointers
+// fmt.Println(&x)
+// y:= &x
+// *y = 8
