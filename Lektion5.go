1f8cf3538fd7fdbc23a9bd1d4db2bf0e06c350ef
HampusEriksson <hampuse1@gmail.com>
2022-11-23 12:32:59 +0100

    Lektion 5 done

--- Lektion5.go
+++ Lektion5.go
@@ -1,17 +1,48 @@
+// https://www.youtube.com/watch?v=2ybLD6_2gKM&ab_channel=LowLevelLearning
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func changelist(myslice []int) {
+	myslice[0] = 100
 }
 
-// Mutable - kan ändras
-// int - exempel
-// slice - exempel
-/*var x []int = []int{3, 4, 5}
-y := x // y och x pekar på samma slice
-y[0] = 100
-fmt.Println(y)*/
-// slice och maps beter sig "konstigt" - de pekar på en minnesplats
-// array skapar en kopia
-// Immutable
-//
+func changestring(mystring string) {
+	mystring = "Hampus"
+}
+
+func check_username(username *string) {
+	fmt.Println(*username)
+
+	if strings.Contains(*username, "fan") {
+		*username = "Hello_Kitty"
+	}
+
+}
+func main() {
+	// slice - exempel
+	// slice och maps beter sig "konstigt" - de pekar på en minnesplats
+	var x []int = []int{1, 2, 3}
+	y := x // y och x pekar på samma slice
+	changelist(x)
+	fmt.Println(y, x)
+
+	var liar string = "Johan"
+	changestring(liar)
+	fmt.Println(liar)
+
+	var username string = "fansatanmolarki"
+	check_username(&username)
+	fmt.Println(username)
+
+	var a int = 5
+	b := a
+	b = 100
+	fmt.Println(a, b)
+	fmt.Println(&a)
+
+}
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-def f(x):
-    return x**2
+x = [1, 2, 3]
+y = x
+print(y, x)
+
+y[0] = 100
+print(y, x)
