a55ae59677c61bff12c0fd86826d88a2a6d4625e
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-12-04 15:02:53 +0100

    Stuff

---
+++ stuff.py
@@ -0,0 +1,2 @@
+def welcome():
+    print("Welcome to the program.")
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-def test(a,b):
-    print(a+b)
+def sub(a, b):
+    return a - b
 
-test(5)
+
+sub(1, 2, 3)
+
+"""Vad är en funktion?
+Vad är en parameter?
+Vad är en default-parameter?
+Måste en funktion ha parametrar?
+Måste en funktion returnera något?
+Vad returneras som default?
+Vad är en modul?
+Ge exempel på en modul.
+Hur får vi tillgång till en modul?
+"""
