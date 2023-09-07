d548670c2fd5b2223c2dbfbfa0e2c08548021e67
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-09-07 10:03:09 +0200

    L6 done

--- HT23/Lektion6.go
+++ HT23/Lektion6.go
@@ -1,9 +1,35 @@
+/*
+
+Python exempel klasser:
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def display_student_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}")
+
+# Creating instances of the Student class
+student1 = Student("Alice", 17, "A")
+student2 = Student("Bob", 18, "B")
+
+# Displaying student information
+student1.display_student_info()
+student2.display_student_info()
+
+*/
+
 // Structs and Custom types
 
 package main
 
 import "fmt"
 
+// In Go, a type implicitly satisfies
+// an interface if it implements all the methods
+// declared by that interface.
 type Person interface {
 	changeSchool(newSchool *School)
 }
@@ -25,8 +51,8 @@ type School struct {
 
 // Metoder skrivs genom att:
 // func (variabelnamn Struct-typ) funktionsnamn() returtyp
-func (s Student) over18() bool {
-	if s.age >= 18 {
+func (stu Student) over18() bool {
+	if stu.age >= 18 {
 		return true
 	} else {
 		return false
@@ -49,9 +75,11 @@ func main() {
 	student1 := &Student{"Ludwig", 18, school1}
 	student2 := &Student{"Leon", 17, school1}
 	teacher1 := &Teacher{"Daniel", 62, school1}
-	student1.name = "Hampus"
 	fmt.Println(student1.school.name)
-	fmt.Println(student2.over18())
+	student1.changeSchool(school2)
+	fmt.Println(student1.school.name)
+	fmt.Println(student1.over18())
+
 	School_persons := []Person{student1, student2, teacher1}
 	fmt.Println(School_persons)
 	for _, person := range School_persons {
--- HT23/test.py
+++ HT23/test.py
@@ -1,25 +1,16 @@
-import turtle
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
 
-# Create a turtle screen
-screen = turtle.Screen()
+    def display_student_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}")
 
-# Create a turtle object
-pen = turtle.Turtle()
+# Creating instances of the Student class
+student1 = Student("Alice", 17, "A")
+student2 = Student("Bob", 18, "B")
 
-# Set the background color of the screen
-screen.bgcolor("white")
-
-# Set the pen color using hexadecimal RGB values (Red)
-pen.color(1, 0, 1)  # Red color in hexadecimal
-
-pen.begin_fill()  # Begin filling the shape
-
-# Draw a square
-for _ in range(4):
-    pen.forward(100)  # Move the turtle forward by 100 units
-    pen.right(90)  # Turn the turtle right by 90 degrees
-
-pen.end_fill()  # End filling the shape
-
-# Close the turtle graphics window when clicked
-screen.exitonclick()
+# Displaying student information
+student1.display_student_info()
+student2.display_student_info()
